f5fc26156b463f52bcf96de4508ac9324e6ead7d
Adrien Guilbaud <adrien.guilbaud33@gmail.com>
2015-12-07 15:54:59 +0100

    p

--- TDP4/code/mpi_fox.c
+++ TDP4/code/mpi_fox.c
@@ -18,75 +18,80 @@ void init_cart_comm(MPI_Comm *grid, MPI_Comm *line_comm, int n){
 void init_datatype(int dim, int bloc_size, MPI_Datatype *mpi_type_bloc){
     MPI_Datatype mpi_type_bloc1;
     MPI_Type_vector(bloc_size, bloc_size, bloc_size*dim, MPI_DOUBLE, &mpi_type_bloc1);
-    MPI_Type_create_resized(mpi_type_bloc1, 0,(bloc_size==1)?sizeof(double):(dim-1)*bloc_size*sizeof(double), mpi_type_bloc);
 
+    MPI_Aint a;
+    
+    MPI_Type_extent(mpi_type_bloc1, &a); printf ("bef; %d\n",a/sizeof(double));
+        
+    MPI_Type_create_resized(mpi_type_bloc1, 0,(bloc_size==1)?sizeof(double):bloc_size*sizeof(double), mpi_type_bloc);
+    MPI_Type_extent(*mpi_type_bloc, &a); printf ("aft; %d\n",a/sizeof(double));
     MPI_Type_commit(mpi_type_bloc);
     MPI_Type_free(&mpi_type_bloc1); 
 }
 
 void fox_scatter(const double *A, double *A_loc, int bloc_size, MPI_Datatype *mpi_type_bloc, int dim){
-    int * scounts = malloc(sizeof(int) * dim*dim);
-    int * displs = malloc(sizeof(int) * dim*dim);
-    int k=0;
+  int * scounts = malloc(sizeof(int) * dim*dim);
+  int * displs = malloc(sizeof(int) * dim*dim);
+  int k=0;
 
-    for (int j = 0; j < dim; j++) {
-	for (int i = 0; i < dim; i++) {
-	    scounts[k] = 1;
-	    displs[k] = (j*dim*bloc_size) + i;
-	    k++;
-	}
+  for (int j = 0; j < dim; j++) {
+    for (int i = 0; i < dim; i++) {
+      scounts[k] = 1;
+	displs[k] = (j*dim*bloc_size) + i;
+	k++;
+      }
     }
     
     MPI_Scatterv(A, scounts, displs, *mpi_type_bloc, A_loc, bloc_size*bloc_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     free(scounts);
     free(displs);
-}
+  }
 
-void fox_gather(double *A, double *A_loc, int bloc_size, MPI_Datatype mpi_type_bloc, int dim){
-    int * rcounts = malloc(sizeof(int) * dim*dim);
-    int * displs = malloc(sizeof(int) * dim*dim);
-    int k=0;
+    void fox_gather(double *A, double *A_loc, int bloc_size, MPI_Datatype mpi_type_bloc, int dim){
+      int * rcounts = malloc(sizeof(int) * dim*dim);
+      int * displs = malloc(sizeof(int) * dim*dim);
+      int k=0;
 
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if(rank==0){
+      int rank;
+      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+      if(rank==0){
 	for (int j = 0; j < dim; j++) {
-	    for (int i = 0; i < dim; i++) {
-		rcounts[k] = 1;
-		displs[k] = (j*dim*bloc_size) + i;
-		k++;
-	    }
+	  for (int i = 0; i < dim; i++) {
+	    rcounts[k] = 1;
+	    displs[k] = (j*dim*bloc_size) + i;
+	    k++;
+	  }
 	}
+      }
+      MPI_Gatherv(A_loc, bloc_size*bloc_size, MPI_DOUBLE, A, rcounts, displs, mpi_type_bloc, 0, MPI_COMM_WORLD);
+
+      free(rcounts);
+      free(displs);
     }
-    MPI_Gatherv(A_loc, bloc_size*bloc_size, MPI_DOUBLE, A, rcounts, displs, mpi_type_bloc, 0, MPI_COMM_WORLD);
 
-    free(rcounts);
-    free(displs);
-}
+    void fox_compute(const int loc_N, const int dim, 
+		     const double *loc_A, const double *loc_B, double *loc_C, 
+		     const int rank, MPI_Comm grid_comm, MPI_Comm line_comm){
 
-void fox_compute(const int loc_N, const int dim, 
-		 const double *loc_A, const double *loc_B, double *loc_C, 
-		 const int rank, MPI_Comm grid_comm, MPI_Comm line_comm){
+      int my_coords[2];
+      int line_rank, num_line, line_root;
+      MPI_Request req_send, req_recv;
+      double *buf_A = malloc(sizeof(double) * loc_N * loc_N);
+      double *buf_B = malloc(sizeof(double) * loc_N * loc_N);
+      memcpy(buf_B, loc_B, sizeof(double) * loc_N * loc_N);
 
-    int my_coords[2];
-    int line_rank, num_line, line_root;
-    MPI_Request req_send, req_recv;
-    double *buf_A = malloc(sizeof(double) * loc_N * loc_N);
-    double *buf_B = malloc(sizeof(double) * loc_N * loc_N);
-    memcpy(buf_B, loc_B, sizeof(double) * loc_N * loc_N);
-
-    /*Getting the rank of a each proc in its line*/ 
-    MPI_Cart_coords(grid_comm, rank, dim, my_coords);
-    num_line = my_coords[1];    
-    my_coords[1] = 0;
-    MPI_Cart_rank(line_comm, my_coords, &line_rank);
+      /*Getting the rank of a each proc in its line*/ 
+      MPI_Cart_coords(grid_comm, rank, dim, my_coords);
+      num_line = my_coords[1];    
+      my_coords[1] = 0;
+      MPI_Cart_rank(line_comm, my_coords, &line_rank);
     
-    for (int k = 0; k < dim; k++) {
+      for (int k = 0; k < dim; k++) {
 	/*The sender for this step copy its local submatrix A in a buffer*/
 	line_root =  (num_line+k)%dim;		
 	if(line_root==line_rank)
-	    memcpy(buf_A, loc_A, sizeof(double) * loc_N * loc_N);
+	  memcpy(buf_A, loc_A, sizeof(double) * loc_N * loc_N);
 
 	MPI_Bcast(buf_A, loc_N*loc_N, MPI_DOUBLE, line_root, line_comm);
 
@@ -104,55 +109,62 @@ void fox_compute(const int loc_N, const int dim,
 	MPI_Wait(&req_recv, MPI_STATUS_IGNORE);
 	
 	//MPI_Sendrecv_replace(buf_B, loc_N*loc_N, MPI_DOUBLE, shift_dst, 0, shift_src, 0, grid_comm, MPI_STATUS_IGNORE);
+      }
+      free(buf_A);
+      free(buf_B);
     }
-    free(buf_A);
-    free(buf_B);
-}
 
 
-void fox(const double *A, const double *B, double *C, int loc_N, int dim){
-    int comm_size, comm_rank;
-    double *loc_A, *loc_B, *loc_C;
-    double start, time_scatter, time_gather, time_compute;
-    MPI_Comm grid_comm, line_comm;
-    MPI_Datatype mpi_type_bloc;
+    void fox(const double *A, const double *B, double *C, int loc_N, int dim){
+      int comm_size, comm_rank;
+      double *loc_A, *loc_B, *loc_C;
+      double start, time_scatter, time_gather, time_compute;
+      MPI_Comm grid_comm, line_comm;
+      MPI_Datatype mpi_type_bloc;
     
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);	
+      MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+      MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);	
         
-    /*Broadcast of the size*/
-    MPI_Bcast(&loc_N, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    loc_A = malloc(sizeof(double) * loc_N * loc_N);
-    loc_B = malloc(sizeof(double) * loc_N * loc_N);
-    loc_C = malloc(sizeof(double) * loc_N * loc_N);
+      /*Broadcast of the size*/
+      MPI_Bcast(&loc_N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+      loc_A = malloc(sizeof(double) * loc_N * loc_N);
+      loc_B = malloc(sizeof(double) * loc_N * loc_N);
+      loc_C = malloc(sizeof(double) * loc_N * loc_N);
 
-    /*Grid, communicator & datatype creation*/
-    init_cart_comm(&grid_comm, &line_comm, dim);
-    init_datatype(dim, loc_N, &mpi_type_bloc);
+      /*Grid, communicator & datatype creation*/
+      init_cart_comm(&grid_comm, &line_comm, dim);
+      init_datatype(dim, loc_N, &mpi_type_bloc);
     
-    /*Scatter matrices*/
-    MPI_Barrier(MPI_COMM_WORLD);
-    start = MPI_Wtime();
-    fox_scatter(A ,loc_A, loc_N, &mpi_type_bloc, dim);
-    fox_scatter(B ,loc_B, loc_N, &mpi_type_bloc, dim);
-    fox_scatter(C ,loc_C, loc_N, &mpi_type_bloc, dim);
-    time_scatter = MPI_Wtime() - start;
-    
-    /*Fox algorithm*/
-    start = MPI_Wtime();
-    fox_compute(loc_N, dim, loc_A, loc_B, loc_C, comm_rank, grid_comm, line_comm);
-    time_compute = MPI_Wtime() - start;
+      /*Scatter matrices*/
+      MPI_Barrier(MPI_COMM_WORLD);
+      start = MPI_Wtime();
+      fox_scatter(A ,loc_A, loc_N, &mpi_type_bloc, dim);
 
-    /*Gather result*/
-    start = MPI_Wtime();
-    fox_gather(C ,loc_C, loc_N, mpi_type_bloc, dim);
-    time_gather = MPI_Wtime() - start;
+      char dest[8];
+      sprintf(dest, "%02d.mat", comm_rank);
+      FILE *fp = fopen(dest, "w+");
+      affiche(loc_N, loc_N, loc_A, loc_N, fp);
+      fclose(fp);
     
-    //  printf("%d %f %f %f\n", comm_rank, time_scatter, time_compute, time_gather);
-    if(comm_rank == 0){
+      fox_scatter(B ,loc_B, loc_N, &mpi_type_bloc, dim);
+      fox_scatter(C ,loc_C, loc_N, &mpi_type_bloc, dim);
+      time_scatter = MPI_Wtime() - start;
+    
+      /*Fox algorithm*/
+      start = MPI_Wtime();
+      fox_compute(loc_N, dim, loc_A, loc_B, loc_C, comm_rank, grid_comm, line_comm);
+      time_compute = MPI_Wtime() - start;
+
+      /*Gather result*/
+      start = MPI_Wtime();
+      fox_gather(C ,loc_C, loc_N, mpi_type_bloc, dim);
+      time_gather = MPI_Wtime() - start;
+
+      printf("%d %f %f %f\n", comm_rank, time_scatter, time_compute, time_gather);
+      if(comm_rank == 0){
 	//affiche(N,N,C,N,stdout);
 	
-    }
+      }
 	
-    free(loc_A); free(loc_B); free(loc_C);
-}
+      free(loc_A); free(loc_B); free(loc_C);
+    }
